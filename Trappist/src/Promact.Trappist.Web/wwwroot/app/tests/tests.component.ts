5af4a3c9784febb9d6e58b859a2a8055df6aa359
Niharika Dubey <niharika@promactinfo.com>
2017-03-15 13:01:47 +0530

    generation of random string

--- Trappist/src/Promact.Trappist.Web/wwwroot/app/tests/tests.component.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/tests/tests.component.ts
@@ -9,23 +9,4 @@ import { Http } from "@angular/http";
 })
 
 export class TestsComponent {
-    //test: Test = new Test;
-    //t_list: Test[] = new Array<Test>();
-    //constructor(public http: Http, private route: ActivatedRoute) {
-    //    this.http = http;
-    //    this.http.get("api/tests").subscribe((response) => {
-    //        this.t_list = response.json();
-    //    });
-    //}
-    //AddTest() {
-    //    this.http.post("api/tests", this.test).subscribe((response) => {
-    //        if (response.ok)
-    //            this.t_list.push(response.json());
-    //        this.test = new Test;         
-    //    });
-    //}
-}
-
-//export class Test {
-   
-//}
+ }
