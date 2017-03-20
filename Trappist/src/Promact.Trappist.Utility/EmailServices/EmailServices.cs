804765fbc1032c0c178062011a7b0748bda8105d
Suaprna Acharya <suparna@promactinfo.com>
2017-03-20 15:37:04 +0530

    resolved all errors

--- Trappist/src/Promact.Trappist.Utility/EmailServices/EmailServices.cs
+++ Trappist/src/Promact.Trappist.Utility/EmailServices/EmailServices.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using MimeKit;
-using System.Net.Http;
 using MailKit.Net.Smtp;
 using MailKit.Security;
 
@@ -31,8 +29,8 @@ namespace Promact.Trappist.Utility.EmailServices
                 using (var client = new SmtpClient())
                 {
                     await client.ConnectAsync("webmail.promactinfo.com", 587, SecureSocketOptions.None);
-                    await client.AuthenticateAsync("suparna@promactinfo.com", "6UBracHAju");
-                    client.Send(emailMessage);
+                http://localhost:50805/Account/ResetPassword?userId=712e6d24-b1a4-4183-8115-cacf1286f9bc&code=CfDJ8LI6KkQzxG1BuZLRHzRNFTTfELj2MJrolOzQNDgV8OGb8Wvdgy9ef%2BgQc3icTVo%2B5sO0TElLVGCG57vC9C0aw0R72kvUXr1CPbLkC9lOGLa%2BzGKk3Rc8UKW7KsLDeb9S6wTugWf7jLzofmiQRO%2BcDoBJV%2BjNjc2AhupTFuYYOZzy%2FgHXjXdLu629awz4HuSBTWSZ38CZi4wV7q%2Fi2OFJC3FK1Nql8yfi4sW67Fcsw7KoAJLvoU1xvKhG5NQ%2BXw3nyg%3D%3Dawait client.AuthenticateAsync("suparna@promactinfo.com", "6UBracHAju");
+                client.Send(emailMessage);
                     client.Disconnect(true);
                 }
                 return true;
